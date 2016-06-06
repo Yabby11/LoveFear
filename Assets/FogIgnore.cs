0cdab059225ebeb99366d2e2326c24ddc91d76c4
unknown <jandrews@SAE-K7K34VBU38F.sae.au>
2016-06-06 14:21:29 +1000

    Worked on main crash scene and made cat eyes, box around, and camera improvements. Need to fix movement.

---
+++ Assets/FogIgnore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogIgnore : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+       RenderSettings.fog = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Materials/MovieTexturesNew Folder/MovieTexturePlay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieTexturePlay : MonoBehaviour {
+
+   public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+
+        MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        movie.Play();
+
+        movie.loop = true;
+  
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
